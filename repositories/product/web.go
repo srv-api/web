9b78b71a23d71fb1ac35fa8d60964b9194fbd648
srv-kirim <inbox.kirim@gmail.com>
2026-01-25 16:49:40 +0700

    slug merchant

--- repositories/product/web.go
+++ repositories/product/web.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/srv-api/merchant/entity"
 	dto "github.com/srv-api/web/dto"
-	"github.com/srv-api/web/helpers"
 )
 
 func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
@@ -14,9 +13,12 @@ func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
 
 	offset := (req.Page - 1) * req.Limit
 
-	// =========================
-	// QUERY DATA
-	// =========================
+	sort := req.Sort
+	if sort == "" {
+		sort = "merchant_details.created_at desc"
+	}
+
+	// ================= DATA =================
 	query := r.DB.
 		Model(&entity.MerchantDetail{}).
 		Distinct("merchant_details.id").
@@ -26,7 +28,7 @@ func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
 		Preload("Image").
 		Where("merchant_details.merchant_slug = ?", req.MerchantSlug).
 		Where("products.status = ?", 1).
-		Order(req.Sort).
+		Order(sort).
 		Limit(req.Limit).
 		Offset(offset)
 
@@ -36,9 +38,7 @@ func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
 
 	req.Rows = merchants
 
-	// =========================
-	// COUNT (WAJIB SAMA JOIN-NYA)
-	// =========================
+	// ================= COUNT =================
 	if err := r.DB.
 		Model(&entity.MerchantDetail{}).
 		Joins("JOIN products ON products.merchant_id = merchant_details.id").
@@ -47,16 +47,9 @@ func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
 		Count(&totalRows).Error; err != nil {
 		return RepositoryResult{Error: err}, 0
 	}
-	for i := range merchants {
-		merchants[i].MerchantName = helpers.TruncateString(merchants[i].MerchantName, 47)
-	}
 
-	// =========================
-	// PAGINATION INFO
-	// =========================
 	req.TotalRows = int(totalRows)
 	req.TotalPages = int(math.Ceil(float64(totalRows) / float64(req.Limit)))
-
 	req.FromRow = offset + 1
 	req.ToRow = offset + req.Limit
 	if req.ToRow > int(totalRows) {
