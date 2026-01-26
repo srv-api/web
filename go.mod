3238d0238e09dba74f3594880156f771f04156c1
srv-kirim <inbox.kirim@gmail.com>
2026-01-26 11:07:13 +0700

    slug merchant

--- go.mod
+++ go.mod
@@ -1,22 +1,21 @@
 module github.com/srv-api/web
 
-go 1.24.4
-
-toolchain go1.24.12
+go 1.25.1
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.15.0
-	github.com/srv-api/merchant v0.0.0-20260118100625-2157c2d7a13f
+	github.com/srv-api/merchant v0.0.0-20260125075843-5cd84f6d3be2
 	github.com/srv-api/middlewares v0.0.0-20260118094353-3c21604ff12e
-	github.com/srv-api/util v0.0.0-20260118092709-27b0e77af41d
+	github.com/srv-api/product v0.0.0-20260125091834-fd132c217c98
+	github.com/srv-api/util v0.0.0-20260118105025-cdb334ac3c2f
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
 
 require (
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/h2non/filetype v1.1.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -34,6 +33,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.9.4 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/srv-api/auth v0.0.0-20260118110236-988ac8498d98 // indirect
 	github.com/streadway/amqp v1.1.0 // indirect
 	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- repositories/product/web.go
+++ repositories/product/web.go
@@ -3,56 +3,57 @@ package product
 import (
 	"math"
 
-	"github.com/srv-api/merchant/entity"
+	merchant "github.com/srv-api/merchant/entity"
+	"github.com/srv-api/product/entity"
 	dto "github.com/srv-api/web/dto"
 )
 
 // repositories/product/web.go
 func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
-	var merchants []entity.MerchantDetail
-	var totalRows int64
+	var (
+		merchant  merchant.MerchantDetail
+		products  []entity.Product
+		totalRows int64
+	)
 
 	offset := (req.Page - 1) * req.Limit
-
 	sort := req.Sort
 	if sort == "" {
-		sort = "merchant_details.created_at desc"
+		sort = "products.created_at desc"
 	}
 
-	// ================= DATA =================
-	err := r.DB.
-		Model(&entity.MerchantDetail{}).
-		Joins("JOIN products ON products.merchant_id = merchant_details.id").
-		Preload("Products.Category").
-		Preload("Products", "products.status = ?", 1).
-		Preload("Products.Image").
-		Where("merchant_details.merchant_slug = ?", req.MerchantSlug).
-		Where("products.status = ?", 1).
-		Group("merchant_details.id").
+	// ================= MERCHANT =================
+	if err := r.DB.
+		Where("merchant_slug = ?", req.MerchantSlug).
+		First(&merchant).Error; err != nil {
+		return RepositoryResult{Error: err}, 0
+	}
+
+	// ================= PRODUCTS =================
+	if err := r.DB.
+		Model(&entity.Product{}).
+		Where("merchant_id = ?", merchant.ID).
+		Where("status = ?", 1).
+		Preload("Category").
+		Preload("Image").
 		Order(sort).
 		Limit(req.Limit).
 		Offset(offset).
-		Find(&merchants).Error
-
-	if err != nil {
+		Find(&products).Error; err != nil {
 		return RepositoryResult{Error: err}, 0
 	}
-
-	req.Rows = merchants
 
 	// ================= COUNT =================
-	err = r.DB.
-		Model(&entity.MerchantDetail{}).
-		Joins("JOIN products ON products.merchant_id = merchant_details.id").
-		Where("merchant_details.merchant_slug = ?", req.MerchantSlug).
-		Where("products.status = ?", 1).
-		Group("merchant_details.id").
-		Count(&totalRows).Error
-
-	if err != nil {
+	if err := r.DB.
+		Model(&entity.Product{}).
+		Where("merchant_id = ?", merchant.ID).
+		Where("status = ?", 1).
+		Count(&totalRows).Error; err != nil {
 		return RepositoryResult{Error: err}, 0
 	}
 
+	// ================= PAGINATION =================
+	req.Rows = products
 	req.TotalRows = int(totalRows)
 	req.TotalPages = int(math.Ceil(float64(totalRows) / float64(req.Limit)))
 	req.FromRow = offset + 1
@@ -61,5 +62,11 @@ func (r *productRepository) Web(req *dto.Pagination) (RepositoryResult, int) {
 		req.ToRow = int(totalRows)
 	}
 
-	return RepositoryResult{Result: req}, req.TotalPages
+	return RepositoryResult{
+		Result: map[string]interface{}{
+			"merchant":   merchant,
+			"products":   products,
+			"pagination": req,
+		},
+	}, req.TotalPages
 }
